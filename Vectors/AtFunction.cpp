046745a6bdf6879974021ef4293afcea004a035f
Kavya Joshi <emailkavya03@gmail.com>
2026-05-23 18:23:38 +0530

    Create AtFunction.cpp

---
+++ Vectors/AtFunction.cpp
@@ -0,0 +1,10 @@
+//At function is used to access an element at a particular index
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+  vector <int> myvec = {1,2,3,4,5,6};
+  cout<<myvec.at(2);
+  return 0;
+}
